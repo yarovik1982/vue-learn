eb13139f2f6c1a718eb76a2a1502e3d122ac2668
acunamatata82 <yar.oleg1982@mail.ru>
2025-06-04 00:57:57 +0300

    Updates

--- assets/ContactsPage-BHwPs5Mo.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as t,b as c}from"./index-DOo90KPJ.js";const a={},s={class:"page__title"};function o(n,_){return c(),t("h2",s,"Contacts Page")}const f=e(a,[["render",o]]);export{f as default};
--- assets/products-DjMxiBno.js
+++
@@ -1,1 +0,0 @@
-const l=[{id:"1",label:"html"},{id:"2",label:"css"},{id:"3",label:"js"},{id:"4",label:"vue"}];export{l as p};
